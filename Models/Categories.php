b96a867622eb23d10e071055cc74ac632e50d1c1
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-20 14:39:10 +0100

    Add product and categories class

---
+++ Models/Categories.php
@@ -0,0 +1,11 @@
+<?php
+
+class Categories {
+    public $name;
+
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
--- Models/Product.php
+++ Models/Product.php
@@ -1,16 +1,20 @@
 <?php
 
+require_once __DIR__ . '/Categories.php';
+
 class Product {
     public $name;
     public $image;
     public $description;
     public $price;
+    public $categories;
 
-    public function __construct($name, $image,$description,$price)
+    public function __construct($name, $image,$description,$price,Categories $categories)
     {
         $this->name = $name;
         $this->image = $image;
         $this->description = $description;
         $this->price = $price;
+        $this->categories = $categories;
     }
 }
